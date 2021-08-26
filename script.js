8908865ce5696386139155a59e15904b72c0608a
Ramly23 <88806353+Ramly23@users.noreply.github.com>
2021-08-26 18:31:02 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -4,24 +4,28 @@ var fullImgBox = document.getElementById("fullImgBox");
 function openFullImg(pic){
     fullImgBox.style.display = "flex";
     fullImg.src = pic;
+    body.classList.add('noscroll');
 }
 
 function closeFullImg() {
     fullImgBox.style.display = "none";
+    body.classList.remove('noscroll');
 }
 
-/*
+
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
 const fadeElems = document.querySelectorAll('.has-fade');
 const body = document.querySelector('body');
-const main = document.querySelector('.main');
+const section = document.querySelector('.first-section');
 
 btnHamburger.addEventListener('click', function() {
 
     if(header.classList.contains('open')){
         body.classList.remove('noscroll'); // Close Hamburger Menu
         header.classList.remove('open');
+        section.classList.remove('hide');
+        section.classList.add('show');
         fadeElems.forEach(function(element){
             element.classList.remove('fade-in');
             element.classList.add('fade-out');
@@ -30,11 +34,12 @@ btnHamburger.addEventListener('click', function() {
 
     else {
         body.classList.add('noscroll');
-        header.classList.add('open');;
+        header.classList.add('open');
+        section.classList.add('hide');
+        section.classList.remove('show');
         fadeElems.forEach(function(element){
             element.classList.remove('fade-out');
             element.classList.add('fade-in');
           });
     }
 });
-*/
