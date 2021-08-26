27be61c85f56a0029998f54cf195b898f370cc97
Ramly23 <88806353+Ramly23@users.noreply.github.com>
2021-08-26 12:45:32 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-const btnHamburger = document.querySelector('#btnHamburger');
-const header = document.querySelector('.header');
-const fadeElems = document.querySelectorAll('.has-fade');
-const body = document.querySelector('body');
-const main = document.querySelector('.main');
-
-btnHamburger.addEventListener('click', function() {
-
-    if(header.classList.contains('open')){
-        body.classList.remove('noscroll'); // Close Hamburger Menu
-        header.classList.remove('open');
-        main.classList.remove('show');
-        fadeElems.forEach(function(element){
-            element.classList.remove('fade-in');
-            element.classList.add('fade-out');
-          });       
-      }
-
-    else {
-        body.classList.add('noscroll');
-        header.classList.add('open');
-        main.classList.add('show');
-        fadeElems.forEach(function(element){
-            element.classList.remove('fade-out');
-            element.classList.add('fade-in');
-          });
-    }
-});
-
 var fullImg = document.getElementById("fullImg");
 var fullImgBox = document.getElementById("fullImgBox");
 
@@ -38,3 +9,32 @@ function openFullImg(pic){
 function closeFullImg() {
     fullImgBox.style.display = "none";
 }
+
+/*
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const fadeElems = document.querySelectorAll('.has-fade');
+const body = document.querySelector('body');
+const main = document.querySelector('.main');
+
+btnHamburger.addEventListener('click', function() {
+
+    if(header.classList.contains('open')){
+        body.classList.remove('noscroll'); // Close Hamburger Menu
+        header.classList.remove('open');
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+          });       
+      }
+
+    else {
+        body.classList.add('noscroll');
+        header.classList.add('open');;
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+          });
+    }
+});
+*/
